e395986b98c5456318f03e71c468165ec1bf3285
Michael.Huynh <michaelhuynh@DET-MHUYNH-MAC.local>
2017-04-13 16:24:09 -0400

    added vin search feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,27 @@ class ApplicationController < ActionController::Base
 
 	def scrape_reddit
 	    require 'open-uri'
-	    doc = Nokogiri::HTML(open("https://www.cars.com/for-sale/searchresults.action/?rd=30&zc=&searchSource=QUICK_FORM&moveTo=listing-698591215"))
+	    #doc = Nokogiri::HTML(open("https://www.cars.com/for-sale/searchresults.action/?rd=30&zc=&searchSource=QUICK_FORM&moveTo=listing-698591215"))
+		vin = "SCA664S55HUX54239"
+	    #doc2 = Nokogiri::HTML(open("https://www.cargurus.com/Cars/instantMarketValueFromVIN.action?startUrl=%2F&carDescription.vin=#{search}"))
+		#entries = doc2.css('.cg-listing-body')
+
+		
+
+		#@prices = Entry.search(params[:search])
+		@pricesArray = []
+		search = params[:search]
+		if doc2 = Nokogiri::HTML(open("https://www.cargurus.com/Cars/instantMarketValueFromVIN.action?startUrl=%2F&carDescription.vin=#{search}"))
+			prices = doc2.css('.cg-listing-body')
+			prices.each do |price|
+				title = price.css('span')[3].text.strip
+				link = price.css('span')[0].text.strip
+				@pricesArray.push([title,link])
+			end
+		end
+
+		##LISTING
+		doc = Nokogiri::HTML(open("https://www.cars.com/for-sale/searchresults.action/?rd=30&zc=&searchSource=QUICK_FORM&moveTo=listing-698591215"))
 
 	    entries = doc.css('.shop-srp-listings__listing')
 	    @entriesArray = []
@@ -33,10 +53,50 @@ class ApplicationController < ActionController::Base
 		  #@entriesArray << Entry.create!(title: title, description: link)
 
 
+end
+
+
+
+=begin
+<table>
+<% Entry.all.each do |entry| %>
+	
+	<tr>
+  		<td><%= entry.title %></td><td><%= entry.description %></td>
+  	</tr>
+<% end %>
+</table>
+
+	    #vin = "SCA664S55HUX54239"
+	    #doc2 = Nokogiri::HTML(open("https://www.cargurus.com/Cars/instantMarketValueFromVIN.action?startUrl=%2F&carDescription.vin=#{vin}"))
+
+	    entries = doc2.css('.cg-listing-body')
+	    #doc.css('.shop-srp-listings__listing')
+	    @entriesArray = []
+	    entries.each do |entry|
+	      title = entry.css('span')[3].text.strip
+	      # entry.css('.listing-row__title>a').text.strip
+	      link = entry.css('.listing-row__price').text.strip
+	      #entry.css('p.title>a')[0]['href']
+	      #@entriesArray << Entry.new(title, link)
+
+	      newEntry = Entry.new(title: title, description:link)
+	      if Entry.exists?(title: title) || Entry.exists?(description: link)
+	      	print 'do nothing'
+	      else
+	      	#newEntry.save
+	      	 newEntry.save
+		  end
+		  #@entriesArray << Entry.create!(title: title, description: link)
+
+
 	    end
+=end	 
 
 	    render template: 'scrape_reddit'
 	  end
+
+
 
 
 
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,18 @@
 class Entry < ApplicationRecord
+
+
+
+	
+	def self.search(search)
+		require 'open-uri'
+		vin = "SCA664S55HUX54239"
+	    doc2 = Nokogiri::HTML(open("https://www.cargurus.com/Cars/instantMarketValueFromVIN.action?startUrl=%2F&carDescription.vin=#{search}"))
+		entries = doc2.css('.cg-listing-body')
+
+		
+		where("title LIKE ?", "%#{search}%") 
+
+	end
+
+
 end
