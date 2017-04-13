cf0cdc3fc1fbfad21e0082bf86440243561c1b2a
Michael.Huynh <michaelhuynh@DET-MHUYNH-MAC.local>
2017-04-13 13:13:49 -0400

    scraping cars.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,14 @@ class ApplicationController < ActionController::Base
 
 	def scrape_reddit
 	    require 'open-uri'
-	    doc = Nokogiri::HTML(open("https://www.reddit.com/"))
+	    doc = Nokogiri::HTML(open("https://www.cars.com/for-sale/searchresults.action/?rd=30&zc=&searchSource=QUICK_FORM&moveTo=listing-698591215"))
 
-	    entries = doc.css('.entry')
+	    entries = doc.css('.shop-srp-listings__listing')
 	    @entriesArray = []
 	    entries.each do |entry|
-	      title = entry.css('p.title>a').text
-	      link = entry.css('p.title>a')[0]['href']
+	      title = entry.css('.listing-row__title>a').text.strip
+	      link = entry.css('.listing-row__price').text.strip
+	      #entry.css('p.title>a')[0]['href']
 	      #@entriesArray << Entry.new(title, link)
 
 	      newEntry = Entry.new(title: title, description:link)
@@ -27,7 +28,7 @@ class ApplicationController < ActionController::Base
 	      	print 'do nothing'
 	      else
 	      	#newEntry.save
-	      	 @entriesArray << newEntry.save
+	      	 newEntry.save
 		  end
 		  #@entriesArray << Entry.create!(title: title, description: link)
 
