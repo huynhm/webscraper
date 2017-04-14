25d661db4c20bc7caa18c606defdc86a5c493821
Michael.Huynh <michaelhuynh@det-rittner.perficient.com>
2017-04-14 11:48:37 -0400

    see all listings feature and add unseen VIN car from search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,78 +23,56 @@ class ApplicationController < ActionController::Base
 		#@prices = Entry.search(params[:search])
 		@pricesArray = []
 		search = params[:search]
-		if doc2 = Nokogiri::HTML(open("https://www.cargurus.com/Cars/instantMarketValueFromVIN.action?startUrl=%2F&carDescription.vin=#{search}"))
+		searchURL = "https://www.cargurus.com/Cars/instantMarketValueFromVIN.action?startUrl=%2F&carDescription.vin=#{search}"
+		if doc2 = Nokogiri::HTML(open(searchURL))
 			prices = doc2.css('.cg-listing-body')
 			prices.each do |price|
-				title = price.css('span')[3].text.strip
-				link = price.css('span')[0].text.strip
+				title = price.css('span')[0].text.strip
+				link = price.css('span')[3].text.strip
+				link = link[6..-1].strip
 				@pricesArray.push([title,link])
+
+				newPrice = Entry.new(title: title, description: link)
+				if Entry.exists?(title: title) && Entry.exists?(description: link)
+			      	#print 'do nothing'
+			    else
+			      	#newEntry.save
+			      	newPrice.save
+				end
 			end
 		end
 
 		##LISTING
-		doc = Nokogiri::HTML(open("https://www.cars.com/for-sale/searchresults.action/?rd=30&zc=&searchSource=QUICK_FORM&moveTo=listing-698591215"))
+		lastPage = 2
+		if params[:seeAll].present?
+			lastPage = 50
+		end
+		$pageCount = 1
+		while $pageCount < lastPage do
+			page = $pageCount.to_s
+			doc = Nokogiri::HTML(open("https://www.cars.com/for-sale/searchresults.action/?page=#{page}&perPage=100&rd=30&searchSource=UTILITY&sf1Dir=DESC&sf1Nm=price&zc=48152"))
 
-	    entries = doc.css('.shop-srp-listings__listing')
-	    @entriesArray = []
-	    entries.each do |entry|
-	      title = entry.css('.listing-row__title>a').text.strip
-	      link = entry.css('.listing-row__price').text.strip
-	      #entry.css('p.title>a')[0]['href']
-	      #@entriesArray << Entry.new(title, link)
+		    entries = doc.css('.shop-srp-listings__listing')
+		    @entriesArray = []
+		    entries.each do |entry|
+		      title = entry.css('.listing-row__title>a').text.strip
+		      link = entry.css('.listing-row__price').text.strip
+		      #entry.css('p.title>a')[0]['href']
+		      #@entriesArray << Entry.new(title, link)
 
-	      newEntry = Entry.new(title: title, description:link)
-	      if Entry.exists?(title: title) || Entry.exists?(description: link)
-	      	print 'do nothing'
-	      else
-	      	#newEntry.save
-	      	 newEntry.save
-		  end
-		  #@entriesArray << Entry.create!(title: title, description: link)
-
-
-end
-
-
-
-=begin
-<table>
-<% Entry.all.each do |entry| %>
-	
-	<tr>
-  		<td><%= entry.title %></td><td><%= entry.description %></td>
-  	</tr>
-<% end %>
-</table>
-
-	    #vin = "SCA664S55HUX54239"
-	    #doc2 = Nokogiri::HTML(open("https://www.cargurus.com/Cars/instantMarketValueFromVIN.action?startUrl=%2F&carDescription.vin=#{vin}"))
-
-	    entries = doc2.css('.cg-listing-body')
-	    #doc.css('.shop-srp-listings__listing')
-	    @entriesArray = []
-	    entries.each do |entry|
-	      title = entry.css('span')[3].text.strip
-	      # entry.css('.listing-row__title>a').text.strip
-	      link = entry.css('.listing-row__price').text.strip
-	      #entry.css('p.title>a')[0]['href']
-	      #@entriesArray << Entry.new(title, link)
-
-	      newEntry = Entry.new(title: title, description:link)
-	      if Entry.exists?(title: title) || Entry.exists?(description: link)
-	      	print 'do nothing'
-	      else
-	      	#newEntry.save
-	      	 newEntry.save
-		  end
-		  #@entriesArray << Entry.create!(title: title, description: link)
-
-
-	    end
-=end	 
-
+		      newEntry = Entry.new(title: title, description:link)
+		      if Entry.exists?(title: title) && Entry.exists?(description: link)
+		      	#print 'do nothing'
+		      else
+		      	#newEntry.save
+		      	 newEntry.save
+			  end
+			  #@entriesArray << Entry.create!(title: title, description: link)
+			end
+			$pageCount = $pageCount + 1
+		end
 	    render template: 'scrape_reddit'
-	  end
+	end
 
 
 
