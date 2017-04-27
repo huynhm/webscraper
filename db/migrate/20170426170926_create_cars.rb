dba2db2921e01239a950afb88740893158449804
Michael.Huynh <michaelhuynh@DET-MHUYNH-MAC.local>
2017-04-27 17:01:16 -0400

    create car migration

---
+++ db/migrate/20170426170926_create_cars.rb
@@ -0,0 +1,15 @@
+class CreateCars < ActiveRecord::Migration[5.0]
+  def change
+    create_table :cars do |t|
+      t.string :user
+      t.string :password
+	  	t.string :vin
+	  	t.string :make
+	  	t.string :model
+	  	t.string :year
+	  	t.string :price
+
+      t.timestamps
+    end
+  end
+end
