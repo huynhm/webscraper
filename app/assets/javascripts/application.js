586bbd8ad80a8217d4fc36f083ca2e0b00b5901e
Michael.Huynh <michaelhuynh@DET-MHUYNH-MAC.local>
2017-05-09 18:57:44 -0400

    changed button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+
+$(document).ready(function(){
+	     $('#seeAll').click(function() {
+            $(this).toggle();
+        });
+});
